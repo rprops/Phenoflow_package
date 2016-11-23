340a67796270b3cec4a7a264e15b6276c272f3a8
rprops <ruben.props@ugent.be>
2016-11-23 10:33:04 -0500

    easyGgplot2 to suggests

--- R/FCS_resample.R
+++ R/FCS_resample.R
@@ -11,18 +11,22 @@
 
 FCS_resample <- function(x, sample=0, replace=FALSE){
   sample_distr <- data.frame(counts=fsApply(x,FUN=function(x) nrow(x),use.exprs=TRUE))
-  p1 <- easyGgplot2::ggplot2.histogram(data=sample_distr , xName='counts',
-                          fill="white", color="black",
-                          linetype="longdash",addMeanLine=TRUE, meanLineColor="red",
-                          meanLineType="dashed", meanLineSize=1)+
-    theme_bw() + labs(y="Frequency", title="Original count distribution")
+  ### Make plot if package easyGgplot2 is installed
+  if (requireNamespace("easyGgplot2", quietly = TRUE)) {
+    p1 <- easyGgplot2::ggplot2.histogram(data=sample_distr , xName='counts',
+                                         fill="white", color="black",
+                                         linetype="longdash",addMeanLine=TRUE, meanLineColor="red",
+                                         meanLineType="dashed", meanLineSize=1)+
+      theme_bw() + labs(y="Frequency", title="Original count distribution")
+    print(p1)
+  }
+  else cat(paste0("Install package easyGgplot2 if you want a histogram plot"))
   if(sample==0) sample <- min(flowCore::fsApply(x=x,FUN=function(x) nrow(x),use.exprs=TRUE))
   ## Remove all .fcs files with less observations than the specified sample
   x <- x[fsApply(x=x,FUN=function(x) nrow(x),use.exprs=TRUE)>sample]
   for(i in 1:length(x)){
     flowCore::exprs(x[[i]]) <- flowCore::exprs(x[[i]])[base::sample(1:nrow(flowCore::exprs(x[[i]])), sample, replace=replace),]
   }
-  print(p1)
   cat(paste0("Your samples were randomly subsampled to ",sample," cells"))
   return (x)
 }
