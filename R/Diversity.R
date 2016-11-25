faa579290bde4f968b5879a523d8b6c25701b18c
rprops <ruben.props@ugent.be>
2016-11-24 19:25:50 -0500

    removed weights = keep conservative estimate of error.

--- R/Diversity.R
+++ R/Diversity.R
@@ -14,8 +14,8 @@ Diversity <- function(x, d=4, plot=FALSE, R=999){
   D2.boot <- function(x,i) 1/sum((x[i]/sum(x[i]))^2)
   D1.boot <- function(x,i) exp(-sum((x[i]/sum(x[i]))*log(x[i]/sum(x[i]))))
   x <- x@basis/apply(x@basis, 1, max)
-  cat("\t**Notice** this function will calculate D0, D1 and D2 with errors estimated by 
-      \tbootstrapping. Resampling is weighted according to bin density.\n")
+  cat("\t**Notice** As only frequency data is available, all bins will be resampled equally.
+      \t This is a conservative estimate of the error.\n")
   D=matrix(ncol=3,nrow=nrow(x))
   ### Observed richness
   D0 = apply(x,1,FUN=function(x) {
