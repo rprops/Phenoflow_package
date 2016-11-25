79e5bbbad07ce1a7a0c83baa87d9423fb1751df2
rprops <ruben.props@ugent.be>
2016-11-25 10:21:03 -0500

    small error

--- R/trip.R
+++ R/trip.R
@@ -9,7 +9,7 @@ trip <- function(x,n=3){
   y = c(); s = c()
   j=1
   for(i in seq(1,length(x),by=n)){
-    y[j] = stats::mean(x[i:(i+n-1)])
+    y[j] = base::mean(x[i:(i+n-1)])
     s[j] = stats::sd(x[i:(i+n-1)])
     j=j+1
   }
