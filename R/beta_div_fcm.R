1ab286393106d8d181b08acef4978fa0f3423585
rprops <ruben.props@ugent.be>
2016-12-02 16:25:22 -0500

    updated documentation

--- R/beta_div_fcm.R
+++ R/beta_div_fcm.R
@@ -67,7 +67,7 @@
 #'  plot_beta_fcm(beta)
 #' @export
 
-beta_div_fcm <- function(x, d = 3, n = 1, dist = "bray", k = 2, iter = 100, ord.type = c("NMDS", "PCoA")) {
+beta_div_fcm <- function(x, d = 4, n = 1, dist = "bray", k = 2, iter = 100, ord.type = c("NMDS", "PCoA")) {
   x <- x@basis/apply(x@basis, 1, max)
   input <- matrix(nrow = nrow(x)/n, ncol = ncol(x))
   j <- 1
