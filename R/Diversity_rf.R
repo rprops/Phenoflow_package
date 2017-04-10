b222c46cf79ec31a9f3c11dfbb73a25e995ea890
rprops <ruben.props@ugent.be>
2017-04-10 17:14:53 -0400

    Included cleaning algorithm flowClean in Diversity_rf

--- R/Diversity_rf.R
+++ R/Diversity_rf.R
@@ -14,6 +14,11 @@
 #' @param nbin Resolution of the binning grid. Defaults to 128 bins which corresponds to a 128x128 binning grid.
 #' @param param Parameter vector indicating on which parameters the diversity should be estimated. An example input would be:
 #' c("FL1-H", "FL3-H", "SSC-H", "FSC-H").
+#' @param cleanFCS Indicate whether outlier removal should be conducted prior to diversity assessment (flowClean package). 
+#' Defaults to FALSE. Requires not subsetted flowSet object (i.e. no parameters should be removed or otherwise flowClean functions fail).
+#' Only samples with > 30,000 cells will be cleaned.
+#' @param cleanparam Indices of parameters to be used for removing errant observations. 
+#' Defaults to 9 and 11 for FL1-H and FL3-H on the BD C6 Accuri FCM.
 #' @keywords diversity, fcm, alpha
 #' @examples
 #' ## Full data processing example
@@ -26,45 +31,72 @@
 #' 
 #' # Asinh transform and select parameters of interest (cells were stained with Sybr Green I).
 #' flowData_transformed <- flowCore::transform(flowData,`FL1-H`=asinh(`FL1-H`),
-#'        `SSC-H`=asinh(`SSC-H`), 
-#'        `FL3-H`=asinh(`FL3-H`), 
+#'        `SSC-H`=asinh(`SSC-H`),
+#'        `FL3-H`=asinh(`FL3-H`),
 #'        `FSC-H`=asinh(`FSC-H`))
-#' param=c('FL1-H', 'FL3-H','SSC-H','FSC-H')
-#' flowData_transformed = flowData_transformed[,param]
-#' 
+#' param=c('FL1-H', 'FL3-H','SSC-H','FSC-H', 'Time')
+#'
 #' # Create a PolygonGate for denoising the dataset
 #' # Define coordinates for gate in sqrcut1 in format: c(x,x,x,x,y,y,y,y)
 #' sqrcut1 <- matrix(c(8.75,8.75,14,14,3,7.5,14,3),ncol=2, nrow=4)
 #' colnames(sqrcut1) <- c('FL1-H','FL3-H')
 #' polyGate1 <- flowCore::polygonGate(.gate=sqrcut1, filterId = 'Total Cells')
-#' 
+#'
 #' # Gating quality check
 #' flowViz::xyplot(`FL3-H` ~ `FL1-H`, data=flowData_transformed[1], filter=polyGate1,
 #'          scales=list(y=list(limits=c(0,14)),
 #'          x=list(limits=c(6,16))),
-#'          axis = lattice::axis.default, nbin=125, 
+#'          axis = lattice::axis.default, nbin=125,
 #'          par.strip.text=list(col='white', font=2, cex=2), smooth=FALSE)
-#'  
-#'  # Isolate only the cellular information based on the polyGate1
-#'  flowData_transformed <- flowCore::Subset(flowData_transformed, polyGate1)
-#'  
-#'  # Normalize parameter values to [0,1] interval based on max. value across parameters
-#'  summary <- flowCore::fsApply(x=flowData_transformed,FUN=function(x) apply(x,2,max),use.exprs=TRUE)
-#'  max = max(summary[,1])
-#'  mytrans <- function(x) x/max
-#'  flowData_transformed <- flowCore::transform(flowData_transformed,`FL1-H`=mytrans(`FL1-H`),
-#'          `FL3-H`=mytrans(`FL3-H`), 
+#'
+#' # Isolate only the cellular information based on the polyGate1
+#' flowData_transformed <- flowCore::Subset(flowData_transformed, polyGate1)
+#'
+#' # Save one object with all parameters and one with the subset of parameters
+#' flowData_transformed_all = flowData_transformed
+#' flowData_transformed_subset = flowData_transformed[,param]
+#'
+#' # Normalize parameter values to [0,1] interval based on max. value across parameters
+#' summary <- flowCore::fsApply(x=flowData_transformed_all, FUN=function(x) apply(x, 2, max), use.exprs=TRUE)
+#' max = max(summary[,9])
+#' mytrans <- function(x) x/max
+#' flowData_transformed_all <- flowCore::transform(flowData_transformed_all,`FL1-H`=mytrans(`FL1-H`),
+#'          `FL3-H`=mytrans(`FL3-H`),
 #'          `SSC-H`=mytrans(`SSC-H`),
 #'          `FSC-H`=mytrans(`FSC-H`))
-#'  
-#'  # Calculate diversity for first 5 samples
-#'  Diversity_rf(flowData_transformed[1:5], param = param, R = 3, R.b = 3)
+#'
+#' summary <- flowCore::fsApply(x=flowData_transformed_subset, FUN=function(x) apply(x,2,max), use.exprs=TRUE)
+#' max = max(summary[,1])
+#' mytrans <- function(x) x/max
+#' flowData_transformed_subset <- flowCore::transform(flowData_transformed_subset,`FL1-H`=mytrans(`FL1-H`),
+#'          `FL3-H`=mytrans(`FL3-H`),
+#'          `SSC-H`=mytrans(`SSC-H`),
+#'          `FSC-H`=mytrans(`FSC-H`))
+#'
+#' # Calculate diversity for first 5 samples without cleaning data
+#' Diversity_rf(flowData_transformed_subset[1:5], param = param, R = 3, R.b = 3)
+#'
+#' # Calculate diversity for first 5 samples with cleaning data if needed
+#' # Requires flowSet with all parameters included. Only samples with more than
+#' # 30,000 cells will be evaluated.
+#' Diversity_rf(flowData_transformed_all[1:5], param = param, R = 3, R.b = 3,
+#' cleanFCS = TRUE, cleanparam = c(9,11))
+
 #' @export
 
 Diversity_rf <- function(x, d = 4, R = 100, R.b = 100, bw = 0.01, nbin = 128, 
-                         param) {
+                         param, cleanFCS = FALSE, cleanparam = c(9,11)) {
+
+  if(cleanFCS == TRUE){
+    cat(date(), paste0("--- Using the following parameters for removing errant collection events\n in samples with > 30,000 cells: ", colnames(x)[cleanparam[1]], " ", 
+                       colnames(x)[cleanparam[2]], "\n"))
+    flowCore::fsApply(x = x, FUN = function(x) clean_FCS(x, cleanparam))
+    x <- x[,param]
+    cat(date(), paste0("--- Done with cleaning data\n"))
+  }
+  
   for (i in 1:R) {
-    cat(date(), paste0("---- Starting resample run ", i, "\n"))
+    cat(date(), paste0("--- Starting resample run ", i, "\n"))
     tmp <- FCS_resample(x, rarefy = TRUE, replace = TRUE, progress = FALSE)
     tmp.basis <- flowFDA::flowBasis(tmp, param = param, nbin = nbin, bw = bw, 
                            normalize = function(x) x)
@@ -86,7 +118,19 @@ Diversity_rf <- function(x, d = 4, R = 100, R.b = 100, bw = 0.01, nbin = 128,
   results.m <- do.call(rbind, results.m)
   results <- data.frame(Sample_names = flowCore::sampleNames(x), results.m, 
                         results.sd)
-  cat(date(), paste0("---- Alpha diversity metrics (D0,D1,D2) have been computed after ", 
+  cat(date(), paste0("--- Alpha diversity metrics (D0,D1,D2) have been computed after ", 
                      R, " bootstraps\n"))
   return(results)
+}
+
+clean_FCS <- function(x, cleanparam){
+  if(nrow(exprs(x)) > 30000){
+    GoodCells <- flowClean::clean(x, vectMarkers = cleanparam, nCellCutoff = 500,
+                                  binSize = 0.01, returnVector = TRUE,
+                                  filePrefixWithDir = paste(x@description$FILENAME, "_cleaned.fcs", 
+                                                            sep = ""), ext = "")
+    flowCore::exprs(x) <- flowCore::exprs(x)[GoodCells < 10000, ]
+    
+  }
+  return(x)
 }
