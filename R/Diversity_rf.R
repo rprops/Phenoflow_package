3a608918fd1ae80765369bc869740d5e56dbfdfa
rprops <ruben.props@ugent.be>
2017-09-06 17:20:46 +0200

    added flowAI parameterization

--- R/Diversity_rf.R
+++ R/Diversity_rf.R
@@ -90,9 +90,11 @@ Diversity_rf <- function(x, d = 4, R = 100, R.b = 100, bw = 0.01, nbin = 128,
     sam_names <- flowCore::sampleNames(x) 
     
     # Extract parameters not to base denoising on
-    filter_param <- colnames(x)[!colnames(x) %in% param]
-    filter_param <- filter_param[filter_param != TimeChannel]
-    filter_param # Exclude all scatter information from denoising
+    param_f <- unique(gsub(param, pattern = "-H|-A", replacement = ""))
+    filter_param <- colnames(x)
+    filter_param <- unique(gsub(filter_param, pattern = "-H|-A", replacement = ""))
+    filter_param <- filter_param[!filter_param %in% param_f & filter_param!= TimeChannel]
+    filter_param <- c(filter_param, "FSC", "SSC")# Exclude all scatter information from denoising
     
     # Denoise with flowAI
     x <- flowAI::flow_auto_qc(x, alphaFR = 0.01,
