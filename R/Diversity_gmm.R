96628a534258f7f473c7e226e7ab5ff38709101a
rprops <Ruben.Props@UGent.be>
2019-01-24 16:07:38 +0100

    Add working GMM and maskGMM functions

---
+++ R/Diversity_gmm.R
@@ -0,0 +1,82 @@
+#' Unsupervised calculation of phenotypic diversity on gaussian mixture-based phenotypic fingerprints
+#' 
+#' This function calculates Hill diversity metrics from the mixtures calculated by PhenoGMM.
+#' D0 is calculated as Observed richness. D1 (exponential of Shannon entropy)
+#' and D2 (inverse Simpson index) are respectively Hill order 1 and 2.
+#' Errors for D0, D1 and D2 are calculated by bootstrapping.
+#' 
+#' @param gmm list containing 1) contigency table and 2) the gmm model created by 
+#' the PhenoGMM function
+#' @param R Number of bootstraps to conduct. Defaults to 100
+#' @param verbose Progress of function is reported. Defaults to FALSE
+#' @keywords diversity, fcm, alpha, gmm, PhenoGMM
+#' @importFrom stats sd
+#' @examples 
+#' # Make model on training data
+#' testGMM <- PhenoGMM(flowData_transformed, downsample = 1e3, nG = 128, param = c("FL1-H", "FL3-H"))
+#' # Apply model to unseen/new data
+#' testPred <- PhenoMaskGMM(flowData_transformed, gmm = testGMM)
+#' # Calculate diversity for both contigency tables
+#' Diversity_gmm(testGMM)
+#' Diversity_gmm(testPred)
+#' @export
+
+Diversity_gmm <- function(gmm, R = 100, verbose = FALSE) {
+  gmm <- gmm[[1]]
+  n_samples <-  nrow(gmm)
+  # Matrix for storing data
+  DIV <- matrix(nrow = n_samples, ncol = 6)
+  row.names(DIV) <- gmm$sample_names
+  
+  # Diversity functions
+  D0.boot <- function(x) sum(x != 0)
+  D1.boot <- function(x) exp(-sum(x * log(x)))
+  D2.boot <- function(x) 1/sum((x)^2)
+  
+  # Start resampling
+  for (i in 1:n_samples) {
+    temp.D0 <- c()
+    temp.D1 <- c()
+    temp.D2 <- c()
+    temp.gmm <- gmm[i, , drop = FALSE]
+    if (verbose)
+      cat(
+        paste0(
+          "\tCalculating diversity for sample ",
+          i,
+          "/",
+          n_samples,
+          " --- ",
+          gmm$sample_names[i],
+          "\n"
+        )
+      )
+    for (j in 1:R) {
+      temp <- phyloseq::rarefy_even_depth(
+          phyloseq::otu_table(temp.gmm[,-1], taxa_are_rows = FALSE),
+          verbose = FALSE,
+          replace = TRUE
+        )
+      # Calculate frequencies
+      temp <- temp/sum(temp)
+      # Calculate Diversities
+      temp.D0 <- c(temp.D0, D0.boot(temp))
+      temp.D1 <- c(temp.D1, D1.boot(temp))
+      temp.D2 <- c(temp.D2, D2.boot(temp))
+      # Store diversities at the end of resampling run
+      if (j == R) {
+        DIV[i, 1] <- mean(temp.D0)
+        DIV[i, 2] <- stats::sd(temp.D0)
+        DIV[i, 3] <- mean(temp.D1)
+        DIV[i, 4] <- stats::sd(temp.D1)
+        DIV[i, 5] <- mean(temp.D2)
+        DIV[i, 6] <- stats::sd(temp.D2)
+        remove(temp.D0, temp.D1, temp.D2)
+      }
+    }
+  }
+  DIV <- data.frame(sample_names = rownames(DIV), DIV)
+  colnames(DIV) = c("Sample_names", "D0", "sd.D0", "D1", "sd.D1", "D2", "sd.D2")
+  cat(date(), "\tDone with all", n_samples, "samples\n")
+  return(DIV)
+}
---
+++ R/PhenoGMM.R
@@ -0,0 +1,70 @@
+#' Apply existing GMM-fitted model to new FCS data. 
+#' These data must contain the same parameters as those on which the model was constructed.
+#'
+#' @param fcs_x flowSet object with input data on which the model should be built 
+#' @param downsample Indicate to which sample size individual samples should be downsampled. 
+#' By default no downsampling is performed
+#' @param nG Number of mixtures to use. Defaults to 128.
+#' @importFrom BiocGenerics unique colnames
+#' @keywords fingerprint
+#' @examples
+#' testGMM <- PhenoGMM(flowData_transformed, downsample = 1e3, nG = 128, param = c("FL1-H", "FL3-H"))
+#' testPred <- PhenoMaskGMM(flowData_transformed, gmm = testGMM)
+#' @export
+
+PhenoGMM <- function(fcs_x, param, downsample = 0, nG = 128){
+  # profvis({ # for profiling performance
+  
+  # Select parameters of interest
+  fcs_x <- fcs_x[, param]
+  
+  # Downsample if necessary
+  if(downsample != 0) fcs_x_sb <- Phenoflow::FCS_resample(fcs_x, sample = downsample, replace = TRUE)
+  
+  # Merge all samples 
+  fcs_m <- Phenoflow::FCS_pool(fcs_x_sb, stub = "*")
+  fcs_m <- suppressWarnings(fcs_m[, param])
+  
+  # Register colmeans and colsd for applying the standardization on
+  # new data
+  fcs_m_colM <- flowCore::fsApply(fcs_m, FUN = function(x) colMeans(x), 
+    use.exprs = TRUE)
+  fcs_m_sd <- flowCore::fsApply(fcs_m, FUN = function(x) apply(x, 2, sd), 
+    use.exprs = TRUE)
+  
+  # Standardize data (center & scale)
+  fcs_m <- flowCore::fsApply(fcs_m, FUN = function(x) scale(x), use.exprs = TRUE)
+
+  # Start performing MClust for GMM estimation
+  gmm_clust <- mclust::Mclust(data = fcs_m, G = nG)
+  
+  # Normalize input data and assign cluster allocations
+  fcs_x_t <- flowCore::fsApply(fcs_x, FUN = function(x) {
+    tmp_nm <- base::sweep(x, 2, fcs_m_colM)/c(fcs_m_sd)
+    data.frame(table(mclust::predict.Mclust(gmm_clust, tmp_nm)$classification))
+    }, use.exprs = TRUE, simplify = FALSE)
+  
+  # Make mixture contigency table
+  gmm_pred <- suppressWarnings(dplyr::bind_rows(fcs_x_t, .id = "id") %>%
+    tidyr::spread(Var1, Freq))
+  colnames(gmm_pred)[1] <- "sample_names"
+  
+  # Replace NAs with zeros
+  gmm_pred[is.na(gmm_pred)] <- 0L
+  
+  # Use model to predict cluster abundances of all data
+  fp_return <- list()
+  fp_return[[1]] <- gmm_pred # model applied to input sample data
+  fp_return[[2]] <- gmm_clust # model for future predictions
+  
+  # Add GMM parameters as attributes to data.table object
+  attr(fp_return[[1]], "nG") <- nG
+  attr(fp_return[[1]], "param") <- param
+  attr(fp_return[[1]], "parameter_mean") <- fcs_m_colM
+  attr(fp_return[[1]], "parameter_sd") <- fcs_m_sd
+  
+  # }) # enable for profviz
+  
+  # Return fingerprint object
+  return(fp_return)
+}
