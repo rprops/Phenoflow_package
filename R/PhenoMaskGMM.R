73f3071500377b809d50621cc307ce682f3b68b0
rprops <Ruben.Props@UGent.be>
2019-06-13 09:54:09 +0200

    add scaling option to phenoGMM

--- R/PhenoMaskGMM.R
+++ R/PhenoMaskGMM.R
@@ -2,6 +2,8 @@
 #'
 #' @param fcs_x flowSet object on which to apply the GMM mask
 #' @param gmm model mask used to assign cluster allocations
+#' @param fcs_scale Should data be scaled/normalized by row and column before running GMM? Defaults to FALSE. 
+#' Be aware that if input modelled used rescaled data as input/option this should be set to TRUE for predicting on new data.
 #' @importFrom BiocGenerics unique colnames
 #' @keywords fingerprint
 #' @importFrom mclust predict.Mclust
@@ -12,7 +14,7 @@
 #' testPred <- PhenoMaskGMM(flowData_transformed, gmm = testGMM)
 #' @export
 
-PhenoMaskGMM <- function(fcs_x, gmm){
+PhenoMaskGMM <- function(fcs_x, gmm, fcs_scale = FALSE){
   # profvis({ # for profiling performance
   # Subset to parameters available in model
   fcs_x <- fcs_x[, attributes(gmm[[1]])$param]
@@ -22,16 +24,23 @@ PhenoMaskGMM <- function(fcs_x, gmm){
   fcs_x_colM <- attributes(gmm[[1]])$parameter_mean
   fcs_x_sd <- attributes(gmm[[1]])$parameter_sd
   
-  # Normalize input data and assign cluster allocations
-  fcs_x_t <- flowCore::fsApply(fcs_x, FUN = function(x) {
-    tmp_nm <- base::sweep(x, 2, fcs_x_colM)/c(fcs_x_sd)
-    data.frame(table(mclust::predict.Mclust(gmm[[2]], tmp_nm)$classification))
-  }, use.exprs = TRUE, simplify = FALSE)
+  if(fcs_scale){
+    # Normalize input data and assign cluster allocations
+    fcs_x_t <- flowCore::fsApply(fcs_x, FUN = function(x) {
+      tmp_nm <- base::sweep(x, 2, fcs_x_colM)/c(fcs_x_sd)
+      data.frame(table(mclust::predict.Mclust(gmm[[2]], tmp_nm)$classification))
+    }, use.exprs = TRUE, simplify = FALSE)
+  } else {
+    # Do not normalize input data and assign cluster allocations
+    fcs_x_t <- flowCore::fsApply(fcs_x, FUN = function(x) {
+      data.frame(table(mclust::predict.Mclust(gmm[[2]], x)$classification))
+    }, use.exprs = TRUE, simplify = FALSE)
+  }
   
   # Make mixture contigency table
   gmm_pred <- suppressWarnings(dplyr::bind_rows(fcs_x_t, .id = "id") %>%
     tidyr::spread(Var1, Freq))
-  colnames(gmm_pred)[1] <- "sample_names"
+  colnames(gmm_pred)[1] <- "Sample_names"
   
   # Replace NAs with zeros
   gmm_pred[is.na(gmm_pred)] <- 0L
